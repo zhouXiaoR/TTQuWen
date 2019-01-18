c11abd2ddf38d26f6eca14f031583a8424a2556d
周晓瑞 <zhouxiaorui@duiba.com.cn>
2019-01-18 10:00:01 +0800

    init program

---
+++ TTQuWen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTQuWen
+//
+//  Created by 周晓瑞 on 2019/1/18.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTQuWen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTQuWen
+//
+//  Created by 周晓瑞 on 2019/1/18.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
